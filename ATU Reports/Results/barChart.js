45a493805a415c29acfe180fc76abb7d442e0b80
devesha <darora.4u@gmail.com>
2017-05-03 16:52:38 +0530

    report today 3may

--- ATU Reports/Results/barChart.js
+++ ATU Reports/Results/barChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var s1 = [0,0,0,0,0,0,0,0,1,4];
-                var s2 = [0,0,0,0,0,0,12,0,0,0];
+                var s1 = [0,0,0,0,0,1,4,0,6,6];
+                var s2 = [0,0,0,12,0,0,0,0,6,6];
                 var s3 = [0,0,0,0,0,0,0,0,0,0];
 
-var ticks = [17,18,19,20,21,22,23,24,25,26];    $.jqplot('bar', [s1, s2, s3], {
+var ticks = [20,21,22,23,24,25,26,27,28,29];    $.jqplot('bar', [s1, s2, s3], {
         animate: true,axesDefaults:{min:0,tickInterval: 1},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
